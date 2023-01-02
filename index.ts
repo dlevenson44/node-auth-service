67bda5b790f41fe8ac9f42ee2155a3dcf95b7298
dlevenson44 <daniellevenson4@gmail.com>
2023-01-02 11:33:11 -0500

    setup prettier

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
+import express, { Express, Request, Response } from 'express'
 
-dotenv.config();
+dotenv.config()
 
-const app: Express = express();
-const port = process.env.PORT;
+const app: Express = express()
+const port = process.env.PORT
 
 app.get('/', (_req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+  res.send('Express + TypeScript Server')
+})
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
+})

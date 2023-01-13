dd0a9f02bfc578e6c188728ce1f73e22d7fbad29
dlevenson44 <daniellevenson4@gmail.com>
2023-01-12 21:40:24 -0500

    fix: setup commit custom script

---
+++ commitizen.config.js
@@ -0,0 +1,17 @@
+const custom = require('@digitalroute/cz-conventional-changelog-for-jira/configurable')
+const defaultTypes = require('@digitalroute/cz-conventional-changelog-for-jira/types')
+const pick = require('lodash/pick')
+
+module.exports = custom({
+    types: pick(defaultTypes, ['feat', 'fix', 'refactor', 'revert', 'docs']),
+    jiraMode: false,
+    scopes: [
+        'build',
+        'component',
+        'page',
+        'service',
+        'style',
+        'types',
+        'other',
+    ],
+})

6bb618bda4ddeaddcb5093c8fe3f44030c75d6dd
Dan Levenson <28206797+dlevenson44@users.noreply.github.com>
2023-01-14 09:19:50 -0500

    Setup testing (#2)
    * fix: setup commit custom script
    
    * feat: setup testing, create first test
    
    * feat: update lint config
    
    * fix: lint fix, precommit test script fix

---
+++ __tests__/server/index.spec.ts
@@ -0,0 +1,12 @@
+import request from 'supertest'
+
+import { server } from '../../server'
+
+describe('GET /', () => {
+  it('should return 200 and success message', async () => {
+    await request(server).get('/').expect(200).expect({ data: 'hello world!' })
+  })
+})
+
+
+
--- server/index.ts
+++ server/index.ts
@@ -4,12 +4,14 @@ import express, { Express, Request, Response } from 'express'
 dotenv.config()
 
 const app: Express = express()
-const port = process.env.PORT
+const port = process.env.PORT ?? 8080
 
 app.get('/', (_req: Request, res: Response) => {
-  res.send('Express + TypeScript Server')
+  res.status(200).json({ data: 'hello world!' })
 })
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
 })
+
+export const server = app
